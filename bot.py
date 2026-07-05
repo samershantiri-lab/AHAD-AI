c0ff74b7c74cbf607d2e613830c07db4dd9746f3
samershantiri-lab <samershantiri@gmail.com>
2026-07-05 09:37:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,22 +2,31 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8697535359:AAFmpR08Rc9WW0EH4sEW6Gcg6vVgZJC2XSI")
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("AHAD AI is running 🚀")
+    await update.message.reply_text(
+        "AHAD AI v1.0 is online 🚀"
+    )
+
 
 async def scan(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Scanning market... 🔍")
+    await update.message.reply_text(
+        "🔍 AHAD AI scanning market...\n\nPlease wait..."
+    )
+
 
 def main():
+    print("Starting AHAD AI...")
+
     app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("scan", scan))
 
-    print("Bot is running...")
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
